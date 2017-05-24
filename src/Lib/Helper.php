7e0a32aa12a60d3db892c716f97dea04f6aae11d
heretreeli <heretreeli@163.com>
2017-05-24 18:11:52 +0800

    函数优化+gitignore

--- src/Lib/Helper.php
+++ src/Lib/Helper.php
@@ -3,120 +3,120 @@ namespace Beanbun\Lib;
 
 Class Helper
 {
-	public static $userAgent = [
-		'pc' => [
-			'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
-	        'Mozilla/5.0 (X11; Linux x86_64; rv:29.0) Gecko/20100101 Firefox/29.0',
-	        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36',
-	        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:29.0) Gecko/20100101 Firefox/29.0',
-	        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36',
-	        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/537.75.14',
-	        'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:29.0) Gecko/20100101 Firefox/29.0',
-	        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36',
-	        'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)',
-	        'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
-	        'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0)',
-	        'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)',
-	        'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)',
-	        'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
-	    ],
-	    'android' => [
-	    	'Mozilla/5.0 (Android; Mobile; rv:29.0) Gecko/29.0 Firefox/29.0',
-        	'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36'
-	    ],
-	    'ios' => [
-	    	'Mozilla/5.0 (iPad; CPU OS 7_0_4 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) CriOS/34.0.1847.18 Mobile/11B554a Safari/9537.53',
-        	'Mozilla/5.0 (iPad; CPU OS 7_0_4 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11B554a Safari/9537.53',
-        	'Mozilla/5.0 (iPhone; CPU iPhone OS 8_0_2 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12A366 Safari/600.1.4',
-        	'Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12A366 Safari/600.1.4'
-	    ]
-	];
+    public static $userAgent = [
+        'pc' => [
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36',
+            'Mozilla/5.0 (X11; Linux x86_64; rv:29.0) Gecko/20100101 Firefox/29.0',
+            'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:29.0) Gecko/20100101 Firefox/29.0',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/537.75.14',
+            'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:29.0) Gecko/20100101 Firefox/29.0',
+            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36',
+            'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)',
+            'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)',
+            'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; WOW64; Trident/4.0)',
+            'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; WOW64; Trident/5.0)',
+            'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)',
+            'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko',
+        ],
+        'android' => [
+            'Mozilla/5.0 (Android; Mobile; rv:29.0) Gecko/29.0 Firefox/29.0',
+            'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36'
+        ],
+        'ios' => [
+            'Mozilla/5.0 (iPad; CPU OS 7_0_4 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) CriOS/34.0.1847.18 Mobile/11B554a Safari/9537.53',
+            'Mozilla/5.0 (iPad; CPU OS 7_0_4 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11B554a Safari/9537.53',
+            'Mozilla/5.0 (iPhone; CPU iPhone OS 8_0_2 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12A366 Safari/600.1.4',
+            'Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12A366 Safari/600.1.4'
+        ]
+    ];
 
-	public static function getUrlbyHtml($html, $url)
-	{
-		$pattern = "'<\s*a\s.*?href\s*=\s*([\"\'])?(?(1) (.*?)\\1 | ([^\s\>]+))'isx";
+    public static function getUrlbyHtml($html, $url)
+    {
+        $pattern = "'<\s*a\s.*?href\s*=\s*([\"\'])?(?(1) (.*?)\\1 | ([^\s\>]+))'isx";
         preg_match_all($pattern, $html, $match);
         $match = array_merge($match[2], $match[3]);
-        $hrefs = array_flip(array_flip(array_filter($match)));
+        $hrefs = array_unique($match);
         foreach ($hrefs as $key => $href) {
-        	$hrefs[$key] = self::formatUrl($href, $url);
+            $hrefs[$key] = self::formatUrl($href, $url);
         }
-        return array_flip(array_flip($hrefs));
-	}
+        return array_unique($hrefs);
+    }
 
-	public static function formatUrl($l1, $l2)
-	{
-		if(strlen($l1) > 0){ 
-        	$I1 = str_replace([chr(34), chr(39)], '', $l1);
-	    } else {
-	        return $l1;
-	    }
-	    $url_parsed = parse_url($l2);
-	    $scheme = $url_parsed['scheme'];
-	    if($scheme != '') {
-	        $scheme .= '://';
-	    }
-	    $host = $url_parsed['host'];
-	    $l3 = $scheme.$host;
-	    if(strlen($l3) == 0) {
-	        return $l1;
-	    }
-	    $path = dirname($url_parsed['path']);
-	    if($path[0] == '\\') {
-	        $path = '';
-	    }
-	    $pos = strpos($I1, '#');
-	    if($pos>0) {
-	        $I1 = substr($I1, 0, $pos);
-	    }
-	    //判断类型
-	    if(preg_match("/^(http|https|ftp):(\/\/|\\\\)(([\w\/\\\+\-~`@:%])+\.)+([\w\/\\\.\=\?\+\-~`@\':!%#]|(&)|&)+/i", $I1)) {
-	        return $I1;
-	    } elseif($I1[0] == '/') {
-	        return $I1 = $l3.$I1;
-	    } elseif (substr($I1,0,3) == '../') {//相对路径
-	        while(substr($I1,0,3) == '../') {
-	            $I1 = substr($I1, strlen($I1)-(strlen($I1)-3), strlen($I1)-3);
-	            if(strlen($path) > 0){
-	                $path = dirname($path);
-	            }
-	        }
-	        return $I1 = $path=='/' ? $l3.$path.$I1 : $l3.$path."/".$I1;
-	    } elseif(substr($I1, 0, 2) == './') {
-	        return $I1 = $l3.$path.substr($I1, strlen($I1)-(strlen($I1)-1), strlen($I1)-1);
-	    } elseif(strtolower(substr($I1, 0, 7))=='mailto:'||strtolower(substr($I1, 0, 11))=='javascript:') {
-	        return false;
-	    } else {
-	        return $I1 = $l3.$path.'/'.$I1;
-	    }
-	}
+    public static function formatUrl($l1, $l2)
+    {
+        if (strlen($l1) > 0) {
+            $I1 = str_replace([chr(34), chr(39)], '', $l1);
+        } else {
+            return $l1;
+        }
+        $url_parsed = parse_url($l2);
+        $scheme = $url_parsed['scheme'];
+        if ($scheme != '') {
+            $scheme .= '://';
+        }
+        $host = $url_parsed['host'];
+        $l3 = $scheme . $host;
+        if (strlen($l3) == 0) {
+            return $l1;
+        }
+        $path = dirname($url_parsed['path']);
+        if ($path[0] == '\\') {
+            $path = '';
+        }
+        $pos = strpos($I1, '#');
+        if ($pos > 0) {
+            $I1 = substr($I1, 0, $pos);
+        }
+        //判断类型
+        if (preg_match("/^(http|https|ftp):(\/\/|\\\\)(([\w\/\\\+\-~`@:%])+\.)+([\w\/\\\.\=\?\+\-~`@\':!%#]|(&)|&)+/i", $I1)) {
+            return $I1;
+        } elseif ($I1[0] == '/') {
+            return $I1 = $l3 . $I1;
+        } elseif (substr($I1, 0, 3) == '../') {//相对路径
+            while (substr($I1, 0, 3) == '../') {
+                $I1 = substr($I1, strlen($I1) - (strlen($I1) - 3), strlen($I1) - 3);
+                if (strlen($path) > 0) {
+                    $path = dirname($path);
+                }
+            }
+            return $I1 = $path == '/' ? $l3 . $path . $I1 : $l3 . $path . "/" . $I1;
+        } elseif (substr($I1, 0, 2) == './') {
+            return $I1 = $l3 . $path . substr($I1, strlen($I1) - (strlen($I1) - 1), strlen($I1) - 1);
+        } elseif (strtolower(substr($I1, 0, 7)) == 'mailto:' || strtolower(substr($I1, 0, 11)) == 'javascript:') {
+            return false;
+        } else {
+            return $I1 = $l3 . $path . '/' . $I1;
+        }
+    }
 
-	public static function getDomain($url)
-	{
-		$parseUrl = parse_url($url);
-		$domain = $parseUrl['scheme'].'://';
+    public static function getDomain($url)
+    {
+        $parseUrl = parse_url($url);
+        $domain = $parseUrl['scheme'] . '://';
 
-		return $domain;
-	}
+        return $domain;
+    }
 
-	public static function randUserAgent($type = 'pc')
-	{
-		switch (self::$userAgent) {
-			case 'pc':
-				return self::$userAgentArray['pc'][array_rand(self::$userAgentArray['pc'])].rand(0, 10000);
-				break;
-			case 'android':
-				return self::$userAgentArray['android'][array_rand(self::$userAgentArray['android'])].rand(0, 10000);
-				break;
-			case 'ios':
-				return self::$userAgentArray['ios'][array_rand(self::$userAgentArray['ios'])].rand(0, 10000);
-				break;
-			case 'mobile':
-				$userAgentArray = array_merge(self::$userAgentArray['android'], self::$userAgentArray['ios']);
-				return $userAgentArray[array_rand($userAgentArray)].rand(0, 10000);
-			default:
-				return $type;
-				break;
-		}
-	}
+    public static function randUserAgent($type = 'pc')
+    {
+        switch (self::$userAgent) {
+            case 'pc':
+                return self::$userAgentArray['pc'][array_rand(self::$userAgentArray['pc'])] . rand(0, 10000);
+                break;
+            case 'android':
+                return self::$userAgentArray['android'][array_rand(self::$userAgentArray['android'])] . rand(0, 10000);
+                break;
+            case 'ios':
+                return self::$userAgentArray['ios'][array_rand(self::$userAgentArray['ios'])] . rand(0, 10000);
+                break;
+            case 'mobile':
+                $userAgentArray = array_merge(self::$userAgentArray['android'], self::$userAgentArray['ios']);
+                return $userAgentArray[array_rand($userAgentArray)] . rand(0, 10000);
+            default:
+                return $type;
+                break;
+        }
+    }
 }
